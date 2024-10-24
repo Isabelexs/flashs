bb0eee75f72fd8f44a72cc66dea1fd0015f9e612
Isa <144124094+Isabelexs@users.noreply.github.com>
2024-10-24 10:43:03 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# flashs
+## CORES
+:root {
+    --text-color: pink;
+    --card-front-color: hot rgb(255, 0, 140);
+--card-back-color: #ffffff;
+}
---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sobre mim:',
+    'Qual meu nome?',
+    'Isabele'
+)
+
+criaCartao(
+    'Sobre mim:',
+    'Qual minha música favorita?',
+    'Oriente-Vagabundo Também Ama'
+)
+
+criaCartao(
+    'Sobre mim:',
+    'Quando é meu aniversário?',
+    '20/02'
+)
+
+criaCartao(
+    'Sobre mim:',
+    'Qual país tenho vontade de conhecer?',
+    'Grécia'
+)
